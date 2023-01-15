b76f8f39f74e0e53f2b0e7c1278ffb78795dbf2d
zsaylors <zsaylors11@gmail.com>
2023-01-14 23:16:20 -0700

    finished LED sensor project.

--- faded lights/faded_lights/faded_lights.ino
+++ faded lights/faded_lights/faded_lights.ino
@@ -1,9 +1,62 @@
-void setup() {
-  // put your setup code here, to run once:
+// Pins for Pulse Width Modulation
+const int redLEDPin = 10;
+const int greenLEDPin = 9;
+const int blueLEDPin = 11;
 
+// Sensor Pins
+const int redSensorPin = A0;
+const int greenSensorPin = A1;
+const int blueSensorPin = A2;
+
+// init color values
+int redValue = 0;
+int greenValue = 0;
+int blueValue = 0;
+
+int redSensorValue = 0;
+int greenSensorValue = 0;
+int blueSensorValue = 0;
+
+void setup() {
+  // comunication rate of 9600 bps
+  Serial.begin(9600);
+
+  pinMode(redLEDPin, OUTPUT);
+  pinMode(greenLEDPin, OUTPUT);
+  pinMode(blueLEDPin, OUTPUT);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  // Get color values from phototransistor
+  redSensorValue = analogRead(redSensorPin);
+  delay(5);
+  greenSensorValue = analogRead(greenSensorPin);
+  delay(5);
+  blueSensorValue = analogRead(blueSensorPin);
+  delay(5);
 
+  // print values from phototransistor
+  Serial.print("\n\nRaw Sensor Values \t red: ");
+  Serial.print(redSensorValue);
+  Serial.print("\t green: ");
+  Serial.print(greenSensorValue);
+  Serial.print("\t blue: ");
+  Serial.print(blueSensorValue);
+
+  // set rgb values
+  redValue = redSensorValue/4;
+  greenValue = greenSensorValue/4;
+  blueValue = blueSensorValue/4;
+
+  // print rgb values
+  Serial.print("\nMapped Sensor Values \t red: ");
+  Serial.print(redValue);
+  Serial.print("\t green: ");
+  Serial.print(greenValue);
+  Serial.print("\t blue: ");
+  Serial.print(blueValue);
+
+  analogWrite(redLEDPin, redValue);
+  analogWrite(greenLEDPin, greenValue);
+  analogWrite(blueLEDPin, blueValue);
 }
